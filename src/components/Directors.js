620452a631746cb0402b6992071673386854f994
Zach <shaw.zacht@gmail.com>
2022-04-20 21:59:12 -0700

    commit tree 28f6e3e347267564bc4e1f7796e7d4c74bace778

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { directors } from "../data";
 
+console.log(directors)
+
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const renderDirectors = directors.map((director) => {
+    return (
+      <div key={director.name}>
+        <h3>{director.name}</h3>
+        <ul>
+          {director.movies.map((movie) => <li key={movie}>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {renderDirectors}
+  </div>
+    );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,27 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  console.log(movies)
+
+  const renderMovies = movies.map((movie) => {
+    return (
+    <div key={movie.title}>
+      <h3>{movie.title}</h3>
+      <p>{movie.time}</p>
+      <ul>
+        {movie.genres.map((genre) => <li key={genre}>{genre}</li>)}
+      </ul>
+    </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {renderMovies}
+    </div>
+    );
 }
 
 export default Movies;
